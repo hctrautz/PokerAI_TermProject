ff10b229042a3156ab1f12212e55ba811629c53e
Hunter Trautz <hctrautz@wpi.edu>
2019-12-05 15:13:27 -0500

    Base Commit

---
+++ StartGame.py
@@ -0,0 +1,9 @@
+from pypokerengine.api.game import setup_config, start_poker
+from examples.players import fish_player
+
+config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
+config.register_player(name="p1", algorithm=fish_player.FishPlayer())
+config.register_player(name="p2",algorithm=fish_player.FishPlayer())
+config.register_player(name="p3", algorithm=fish_player.FishPlayer())
+game_result = start_poker(config, verbose=1)
+print(game_result)
