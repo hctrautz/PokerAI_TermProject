899775aafcaac87ad4b8e3128b1bcf1015c91568
Hunter Trautz <hctrautz@wpi.edu>
2019-12-06 11:35:00 -0500

    Create MonteCarlo_player_setup.py

---
+++ MonteCarlo_player_setup.py
@@ -0,0 +1,38 @@
+from pypokerengine.players import BasePokerPlayer
+from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate
+
+NB_SIMULATION = 1000
+
+class MonteCarloPlayer(BasePokerPlayer):  # Do not forget to make parent class as "BasePokerPlayer"
+
+    #  we define the logic to make an action through this method. (so this method would be the core of your AI)
+    def declare_action(self, valid_actions, hole_card, round_state):
+        community_card = round_state['community_card']
+        win_rate = estimate_hole_card_win_rate(
+            nb_simulation=NB_SIMULATION,
+            nb_player=self.nb_player,
+            hole_card=gen_cards(hole_card),
+            community_card=gen_cards(community_card)
+        )
+        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
+        call_action_info = valid_actions[1]
+        action, amount = call_action_info["action"], call_action_info["amount"]
+        return action, amount   # action returned here is sent to the poker engine
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+def setup_ai():
+    return MonteCarloPlayer()
